66209722fb0e6f801d68253f13eb9dc1684f94c5
Aditya <114207772+programmaraditya123@users.noreply.github.com>
2026-01-28 14:53:21 +0530

    Update page.tsx

--- frontend/app/vibes/page.tsx
+++ frontend/app/vibes/page.tsx
@@ -4,11 +4,11 @@ import Sidebar from "@/src/components/HomePage/Sidebar/Sidebar";
 import UnderDevelopment from "@/src/components/UnderDevelopment/UnderDevelopment";
 import { useState } from "react";
 
-export const metadata = {
-  title: "Watch Short Videos – Vibes by VidoraHub",
-  description:
-    "Watch short videos, trending reels and viral clips on Vibes by VidoraHub. A new way to explore creators, stories and entertainment in seconds."
-};
+// export const metadata = {
+//   title: "Watch Short Videos – Vibes by VidoraHub",
+//   description:
+//     "Watch short videos, trending reels and viral clips on Vibes by VidoraHub. A new way to explore creators, stories and entertainment in seconds."
+// };
 
 export default function VibesPage() {
   const [development, setDevelopment] = useState(1);
