df77859788a78f3b7e559f67d854bf9edbee8e4f
cipher-preet <ps1535146@gmail.com>
2026-04-26 11:43:02 +0530

    add offline.html in public folder and prepare a new relaese for google play console 26-04-2026 11:42AM

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.vidorahub.app',
+  appId: 'com.vidorahub.mobile',
   appName: 'VidoraHub',
   webDir: 'public',
 
